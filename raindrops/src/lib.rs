1ccca45c57d44ddfe5c1de46c8ab46f08b6a4376
Carl Englund <englund92@gmail.com>
2017-02-13 22:18:22 +0100

    Raindrops problem

--- raindrops/src/lib.rs
+++ raindrops/src/lib.rs
@@ -1,3 +1,21 @@
-pub fn raindrops(number: i32) => String {
+pub fn raindrops(number: i32) -> String {
+   let is_pling = |number| number % 3 == 0; 
+   let is_plang = |number| number % 5 == 0; 
+   let is_plong = |number| number % 7 == 0; 
+   let mut return_string = String::new();
 
+   if is_pling(number) {
+       return_string.push_str("Pling");
+   }
+   if is_plang(number) {
+       return_string.push_str("Plang");
+   }
+   if is_plong(number) {
+       return_string.push_str("Plong");
+   }
+   if return_string.is_empty() {
+       let s = format!("{}", number);
+       return_string.push_str(&s);
+   }
+   return_string
 }
